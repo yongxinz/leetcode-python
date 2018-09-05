6a760e4aef0ec7c161742497a9a151f670236c54
yongxinz <hi_nature@yeah.net>
2018-09-05 08:53:42 +0800

    two_sum

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# leetcode-python
+# LeetCode solutions in Python3
+
+## 列表详情
+|  #  | 题目           |  解决       |  难度           | 
+|-----|---------------- | --------------- | --------------- | 
+001 | [两数之和](https://leetcode-cn.com/problems/two-sum/description/) | [two_sum.py](./two_sum.py) | 简单       |
---
+++ two_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+if __name__ == '__main__':
+    print(Solution().twoSum((2, 7, 11, 15), 9))
